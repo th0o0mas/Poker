757c5a4a2c9634a37781ef7229f53ef13be133fd
th0o0mas <104323318+th0o0mas@users.noreply.github.com>
2024-10-08 10:31:05 +0200

    Update V1.c

--- V1.c
+++ V1.c
@@ -9,6 +9,7 @@ struct Player
 {
     char *Role; 
     char *Nom;
+    char *Etat; /*Nous permet d'y enregistrer si ALL IN, si se couche et si suit ou raise*/
     int Argent;
     int Mise;
 };
@@ -20,7 +21,7 @@ void Affichage_Joueur(Player Joueur)
     printf(" \n Nom : %s, Argent : %d, Role : %s \n ", Joueur.Nom, Joueur.Argent, Joueur.Role);
 }
 
-Player Ajouter_Joueur(char *Name, int Argent, char *Role_g)
+Player Ajouter_Joueur(char *Name, int Argent, char *Role_g, char *Etat_g)
 {
     /* Création d'un joueur */
     Player *Player_cree = malloc(sizeof(*Player_cree));
@@ -37,12 +38,19 @@ Player Ajouter_Joueur(char *Name, int Argent, char *Role_g)
     }
     strcpy(Player_cree->Nom, Name);/*Nous permet de copier Name dans Player_cree*/
 
-    Player_cree->Role = malloc(strlen(Role_g) + 1); /*Caractère arrêt du string d'où le +1*/
+    Player_cree->Role = (char*) malloc(11); /*On prend 11 carac pour enregistrer small blind*/
     if (Player_cree->Role == NULL) {
         fprintf(stderr, "Erreur d'allocation mémoire pour le nom\n");
         exit(EXIT_FAILURE);
     }
     strcpy(Player_cree->Role, Role_g);
+
+    Player_cree->Etat = (char*) malloc(7); /*7 pour ALL IN et moins pour les autres*/
+    if (Player_cree->Etat == NULL) {
+        fprintf(stderr, "Erreur d'allocation mémoire pour le nom\n");
+        exit(EXIT_FAILURE);
+    }
+    strcpy(Player_cree->Etat, Etat_g);
 
     /* Modifications attributs */
     Player_cree->Argent = Argent;
@@ -93,6 +101,7 @@ void Initialisation_Roles(Player Record[6])
 }
 
 
+
 int Affichage_Argent_Pot(Player Record[6], int Joueurs_nombre)
 {
     int Somme;
@@ -100,6 +109,32 @@ int Affichage_Argent_Pot(Player Record[6], int Joueurs_nombre)
         Somme+=Record[i].Mise;
     }
     return Somme;
+}
+
+void Ajouter_Argent(Player Joueurs, int Argent_incremente)
+{
+    Joueurs.Argent += Argent_incremente;
+}
+
+void Mise(Player Record[6], int Joueurs_nombre, int Small_Blind_Actuelle)
+{
+    /* On commence d'abord par mettre les mises de petite blinde et grosse blinde (en s'assurant qu'ils aient assez, sinon ils sont all in et sinon)*/
+    for (int i=0; i<Joueurs_nombre; i++)
+    {
+        if (Record[i].Role=="Small Blind")
+        {
+            Record[i].Mise=Small_Blind_Actuelle;
+            Ajouter_Argent(Record[i],-Small_Blind_Actuelle);
+        }
+        if (Record[i].Role=="Big Blind")
+        {
+            Record[i].Mise=2*Small_Blind_Actuelle;
+            Ajouter_Argent(Record[i],-(2*Small_Blind_Actuelle));
+        }
+    }
+    i++;
+
+
 }
 
 int main()
@@ -120,7 +155,8 @@ int main()
     
     for (int i = 0; i < Joueurs_nombre; i++) {
         free(Record[i].Nom);
-        free(Record[i].Role);  // Libérer de chaque élement allouer dynamiquement ( à ajouter en fin de processus)
+        free(Record[i].Role);
+        free(Record[i].Etat);  // Libérer de chaque élement allouer dynamiquement ( à ajouter en fin de processus)
     }
 
 
